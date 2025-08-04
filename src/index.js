85f1cf1526e8e14afca759b6dce63df0fbb983d6
halleynani <nanihalley@gmail.com>
2025-08-04 18:56:14 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
+function displayPoem(response) {
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      }); 
+}
+
+
+
+
+
+
 function generatePoem(event) {
     event.preventDefault();
+
+    let apiKey = "f074c9b4d4608483fc7edf3a88daoetb";
+    let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiURL).then(displayPoem);
+
+
+
+
+
   
     new Typewriter("#poem", {
       strings: "La tombe dit à la rose",

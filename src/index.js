4b7826c3e2375e6ade5606d3412bfb8f3fd7bfe7
halleynani <nanihalley@gmail.com>
2025-08-04 17:44:04 -0500

    Updated javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 function generatePoem(event) {
     event.preventDefault();
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "La tombe dit a la rose";
+let poemElement = document.querySelector("#poem");
+
+new Typewriter("#poem", {
+    strings: "La tombe dit a la rose",
+    autoStart: true,
+    delay: 1,
+   cursor: "",
+  });
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");

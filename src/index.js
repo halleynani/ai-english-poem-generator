8f7371cbe1f9746a2ab98df0ddd73879551affce
halleynani <nanihalley@gmail.com>
2025-08-04 18:08:31 -0500

    added axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+function displayPoem(response) {
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+}
+
+
+
+
 function generatePoem(event) {
     event.preventDefault();
   
-    new Typewriter("#poem", {
-      strings: "La tombe dit à la rose",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+    let apiKey = "f074c9b4d4608483fc7edf3a88daoetb";
+    let prompt = 
+    let context = 
+    let apiURL = 
+        `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+        
+axios.get(apiURL).then(displayPoem);
   }
   
   let poemFormElement = document.querySelector("#poem-generator-form");

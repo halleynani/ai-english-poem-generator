5cc78b14c662e3c83988018343db449dff2e9d12
halleynani <nanihalley@gmail.com>
2025-08-04 17:46:35 -0500

    Updated html and js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 function generatePoem(event) {
     event.preventDefault();
-
-let poemElement = document.querySelector("#poem");
-
-new Typewriter("#poem", {
-    strings: "La tombe dit a la rose",
-    autoStart: true,
-    delay: 1,
-   cursor: "",
-  });
-}
-
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+  
+    new Typewriter("#poem", {
+      strings: "La tombe dit à la rose",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);

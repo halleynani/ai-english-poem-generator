0d8a093d8f600d866a7f5587c2eebedab822c469
halleynani <nanihalley@gmail.com>
2025-08-04 18:36:50 -0500

    updated javascript

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 function displayPoem(response) {
     new Typewriter("#poem", {
-        strings: response.data.answer,
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
-}
-
-
-
-
-function generatePoem(event) {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  function generatePoem(event) {
     event.preventDefault();
   
-
     let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey = "f074c9b4d4608483fc7edf3a88daoetb";
-    let prompt = 
-    `User instructions: Generate a poem in English about ${instructionsInput.value}`;
-    let context = 
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title with the poem.";
-    let apiURL = 
-        `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-
-
-        
-axios.get(apiURL).then(displayPoem);
+    let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+    let context =
+      "You are a romantic poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating an English poem about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayPoem);
   }
   
   let poemFormElement = document.querySelector("#poem-generator-form");
